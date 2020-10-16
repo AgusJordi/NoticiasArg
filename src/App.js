69660b0816d522ee6e45b97078a5c6af75c466a1
Agustin Jordi Rojas <agusjrojas@gmail.com>
2020-10-15 21:22:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
+
+
+function App() {
+
+
+  //definir la categoria y noticia
+  const [categoria, setCategoria] = useState('');
+  const [noticias, setNoticias] = useState([]);
+
+  
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=3221d766846e48abb6ad00d4caf3141c`
+      
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      setNoticias(noticias.articles);
+    }
+
+
+    consultarAPI();
+  }, [categoria])
+
+  return (
+  <Fragment>
+    <Header 
+      titulo="Buscador de Noticias"
+    />
+
+    <div className="container white">
+
+    <Formulario
+    setCategoria={setCategoria}
+    />
+
+    <ListadoNoticias 
+      noticias={noticias}
+    />
+
+    </div>
+
+  </Fragment>
+  );
+}
+
+
+export default App;
